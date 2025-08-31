f7303d5e4a87c353f2a48eb7b7a176820c08f2b6
nisreen55 <you@example.com>
2025-08-31 02:29:50 -0500

    Connect the front-end and backend.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,10 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello!');
 })
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.send({ message: "Hello from json" });
+});
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
 });
